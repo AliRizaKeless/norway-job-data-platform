164529eb31c3df323585183cadaf91ba0fe733d0
Ali Riza Keles <alirizakeles66@gmail.com>
2026-05-22 22:46:22 +0200

    Convert SSB JSON-stat response into DataFrame

--- src/ssb_test.py
+++ src/ssb_test.py
@@ -1,4 +1,5 @@
 import requests
+import pandas as pd
 
 
 def fetch_ssb_data():
@@ -10,13 +11,33 @@ def fetch_ssb_data():
 
     response = requests.get(url, params=params, timeout=30)
     response.raise_for_status()
+    return response.json()
 
-    data = response.json()
 
-    print("[INFO] SSB data fetch successful")
-    print(type(data))
-    print(data.keys())
+def convert_to_dataframe(data):
+    values = data["value"]
+
+    dimensions = data["id"]
+    sizes = data["size"]
+
+    print("[INFO] Dimensions:", dimensions)
+    print("[INFO] Sizes:", sizes)
+    print("[INFO] Number of values:", len(values))
+
+    df = pd.DataFrame({
+        "value": values
+    })
+
+    return df
+
+
+def main():
+    data = fetch_ssb_data()
+    df = convert_to_dataframe(data)
+
+    print("[INFO] DataFrame created")
+    print(df.head())
 
 
 if __name__ == "__main__":
-    fetch_ssb_data()
+    main()
